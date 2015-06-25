a792e1fef37ad4ac16a9174bbc7e8deacc79d8f6
MadHatterTenSix <beastwood2013@gmail.com>
2015-06-25 14:13:49 -0600

    Update ManglingSentences.java

--- ManglingSentences.java
+++ ManglingSentences.java
@@ -1,26 +1,38 @@
 /**
  *
  * @author __MadHatter (alias used on https://www.reddit.com/r/dailyprogrammer)
+ *
+ * Edited: 2015-06-25
+ *
+ * Converting Unicode characters to ASCII
+ * http://stackoverflow.com/questions/15356716/how-can-i-convert-unicode-string-to-ascii-in-java
  */
 
 /* ManglingSentences.java */
 
+import java.text.Normalizer;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 public class ManglingSentences {
-    
+
     public static void main(String[] args) {
-        Scanner in;
+        int index;
+        Character c;
+        Scanner in = new Scanner(System.in);
         String line;
-        StringBuilder output;
+        String regex = Pattern.quote("[\\p{InCombiningDiacriticalMarks}\\p{IsLm}\\p{IsSk}]+");
+        StringBuilder output = new StringBuilder();
         StringBuilder input;
         StringTokenizer stringTokenizer;
-
-        in = new Scanner(System.in);
+        ArrayList<Integer> listOfCapitals;
+        
         line = in.nextLine();
+        line = Normalizer.normalize(line, Normalizer.Form.NFD);
+        listOfCapitals = getListOfCapitals(line);
 
-        output = new StringBuilder();
         stringTokenizer = new StringTokenizer(line);
         while (stringTokenizer.hasMoreElements()) {
             input = new StringBuilder(stringTokenizer.nextToken());
@@ -28,24 +40,41 @@ public class ManglingSentences {
             output.append(" ");
         }
 
+        for (int i = 0; i < listOfCapitals.size(); i++) {
+            index = listOfCapitals.get(i);
+            c = Character.toUpperCase(output.charAt(index));
+            output.setCharAt(index, c);
+        }
+
         System.out.println(output.toString());
+    }
+
+    public static ArrayList<Integer> getListOfCapitals(String s) {
+        ArrayList<Integer> listOfCapitals;
+
+        listOfCapitals = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                listOfCapitals.add(i);
+            }
+        }
+
+        return listOfCapitals;
     }
 
     public static String sortWord(StringBuilder word) {
         int i;
         int j;
-        boolean isCapitalized;
         char tmpA;
         char tmpB;
 
-        isCapitalized = Character.isUpperCase(word.charAt(0));
-
-        for (i = 1; i < word.length();i++) {
+        for (i = 1; i < word.length(); i++) {
             j = i;
             while (j > 0) {
                 tmpA = Character.toLowerCase(word.charAt(j));
                 tmpB = Character.toLowerCase(word.charAt(j-1));
-                if (tmpB < tmpA || !Character.isAlphabetic(tmpA)) {
+                if (tmpB < tmpA || !isLetterOrDigit(tmpA) || !isLetterOrDigit(tmpB)) {
                     break;
                 }
                 word.setCharAt(j, tmpB);
@@ -54,11 +83,13 @@ public class ManglingSentences {
             }
         }
 
-        if (isCapitalized) {
-            word.setCharAt(0, Character.toUpperCase(word.charAt(0)));
-        }
-
         return word.toString();
+    }
+
+    public static boolean isLetterOrDigit(Character c) {
+        return (c >= 'a' && c <= 'z'
+                || c >= 'A' && c <= 'Z'
+                || c >= '0' && c <= '9');
     }
 
 }
